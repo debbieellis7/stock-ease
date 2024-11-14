ec05f861bee9fe1dc113db8a12a2a3304aca0e18
debbieellis7 <1debbieellis@gmail.com>
2024-11-14 23:39:03 +0800

    Add initial Table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 // Internal components
-import Header from "../components/Header/Index";
+import Header from "@/components/Header";
+import Table from "@/components/Table";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
+  const { theme } = useTheme();
+  const [isClient, setIsClient] = useState(false);
+  const bgColor = theme === "dark" ? "bg-black" : "bg-gray-50";
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-    <div className="p-3">
-      <Card className="flex flex-col shadow-none p-2 border-0">
+    <div className={`poppins p-5 ${bgColor} border w-full min-h-screen`}>
+      <Card className="flex flex-col shadow-none p-5">
         <Header />
+        <Table />
       </Card>
     </div>
   );
---
+++ src/components/Table/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+
+export default function Table() {
+  return (
+    <Card className="mt-12 flex flex-col shadow-none poppins border-none">
+      <CardHeader className="flex justify-between p-2">
+        <div className="flex justify-between items-center">
+          <div className="">
+            <CardTitle className="font-bold text-[23px]">Products</CardTitle>
+            <p className="text-sm text-slate-600">34 products</p>
+          </div>
+          <Button>Add Product</Button>
+        </div>
+      </CardHeader>
+
+      <CardContent></CardContent>
+    </Card>
+  );
+}

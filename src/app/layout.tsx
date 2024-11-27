37c6cad1d9e5d24ed44b8afa2826127d56c589ed
debbieellis7 <1debbieellis@gmail.com>
2024-11-27 21:27:23 +0800

    Disable system theme and set light as default in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       <body className={`${poppins.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
